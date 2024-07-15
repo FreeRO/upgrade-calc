8706ba27e8553b64c5f4a7806c103d520b2f588d
jass <jasswater@gmail.com>
2024-07-15 17:33:42 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <UpgradeCalculator />
+  </main>
+</template>
+
+<script setup lang="ts">
+import UpgradeCalculator from './components/UpgradeCalculator.vue'
+</script>
+
+<style scoped>
+@media (min-width: 1024px) {
+}
+</style>
---
+++ src/components/UpgradeCalculator.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Hellow</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
